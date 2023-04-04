e691f416a99aca35ccc135ab18708ad02387a0ec
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-04 12:32:20 +0900

    app.tsx戻す

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-const App = () => {
-  return (
-    <div className='text-center'>
-      my portfolio
-    </div>
-  )
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-
-export default App;

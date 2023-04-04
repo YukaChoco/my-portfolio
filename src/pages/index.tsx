4357e0d0f339e632b7c0a076b67d904d836e3247
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-04 16:45:12 +0900

    背景の画像設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,42 @@
 import Link from "next/link";
+import Image from 'next/image';
+import profileImage from "../images/profile_img.png";
 
 function Home() {
   return (
-    <div className='text-center w-full h-screen bg-zinc-100'>
+    <div className='text-center w-full h-screen bg-gray-100'>
       Homeです
+      <div className="fixed bottom-0 left-[10vw] h-full w-[100vh] z-0">
+        <Image
+          alt="ProfileImage"
+          src={profileImage}
+          placeholder="blur"
+          quality={100}
+          fill
+          sizes="20vw"
+        />
 
-      <ul>
-        <li>
-          <Link href="/Profile">
-            Profile
-          </Link>
-        </li>
-        <li>
-          <Link href="Contents">
-            Contents
-          </Link>
-        </li>
-        <li>
-          <Link href="/Experience">
-            Experience
-          </Link>
-        </li>
-      </ul>
+      </div>
+      <div className="relative z-50">
+        <ul>
+          <li>
+            <Link href="/Profile">
+              Profile
+            </Link>
+          </li>
+          <li>
+            <Link href="/Contents">
+              Contents
+            </Link>
+          </li>
+          <li>
+            <Link href="/Experience">
+              Experience
+            </Link>
+          </li>
+        </ul>
+
+      </div>
     </div>
   )
 }

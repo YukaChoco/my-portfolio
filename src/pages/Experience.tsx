252891440474fed441a408263ec4ccf998267bb2
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-04 14:41:25 +0900

    ページのRoutingOK

---
+++ src/pages/Experience.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+function Experience() {
+  return (
+    <div className='w-full h-screen bg-zinc-100'>
+      <Header></Header>
+      Experienceです
+    </div>
+  )
+}
+
+export default Experience;
---
+++ src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+function Profile() {
+  return (
+    <div className='w-full h-screen bg-zinc-100'>
+      <Header></Header>
+      Profileです
+    </div>
+  )
+}
+
+export default Profile;

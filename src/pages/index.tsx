ff259c365175f85c9a32bcebae11b50db2e56568
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-08 19:48:59 +0900

    homeデザイン修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,30 +2,51 @@ import Link from "next/link";
 import Image from 'next/image';
 import profileImage from "../images/profile_img.png";
 import HomeButton from "@/components/HomeButton";
+import { useEffect, useState } from "react";
 
 function Home() {
+
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+
+      window.addEventListener("resize", handleResize);
+      handleResize();
+      return () => window.removeEventListener("resize", handleResize);
+    } else {
+      return;
+    }
+  }, []);
+
   return (
-    <div className='text-center w-screen h-screen bg-gray-100 grid grid-cols-5 grid-rows-1 px-[5vw]'>
-      <div className="h-screen z-0 col-start-1 col-span-3 row-start-1">
+    <div className='relative text-center w-screen h-screen bg-gray-100'>
+      <div className="absolute h-screen w-fit z-0 left-[10vw] right-[10vw]">
         <Image
           alt="ProfileImage"
           src={profileImage}
           placeholder="blur"
           quality={100}
-          className="object-contain h-full"
+          className={"object-contain h-full w-fit "+(windowSize.height/windowSize.width>0.6?"opacity-50":"")}
         />
-
       </div>
-      <div className="z-50 col-start-3 col-span-3 row-start-1 justify-center grid grid-row-2 py-[10vh]">
-        <div className='relative text-gray-600 text-5xl font-bold rounded-2xl'>
-          <div className="absolute w-fit h-fit bottom-[5vh]">
+      <div className="absolute right-[10vw] pl-[10vw] h-screen w-fit grid grid-rows-2 z-50 py-[20vh]">
+        <div className={'text-' + (windowSize.height/windowSize.width>0.6?"gray-500":"gray-600") + ' text-' + (windowSize.height/windowSize.width>1.3?"5xl":"[8vw]") +' align-middle font-bold rounded-2xl my-auto'}>
             My Portfolio
-          </div>
         </div>
-        <div className="grid grid-rows-3 pt-[10vh] content-center">
-          <HomeButton pageName="Profile"></HomeButton>
-          <HomeButton pageName="Contents"></HomeButton>
-          <HomeButton pageName="Experience"></HomeButton>
+        <div className="grid grid-rows-3">
+        <HomeButton pageName="Profile"></HomeButton>
+        <HomeButton pageName="Contents"></HomeButton>
+        <HomeButton pageName="Experience"></HomeButton>
         </div>
       </div>
     </div>

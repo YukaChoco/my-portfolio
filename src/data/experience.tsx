b869e8ecaaf0d85ea330e70b8d8e52a6c33a6c8b
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-10 03:50:29 +0900

    experience追加・displaycontentの中身変更

---
+++ src/data/experience.tsx
@@ -0,0 +1,53 @@
+import { Data } from "@/types/global";
+
+const profileData: Data[] = [
+    {
+        theme: '2021',
+        dataList: [
+            {
+                content: '4月',
+                detail: ['立命館大学情報理工学部 入学', '株式会社 ジェイアール西日本フードサービスネット 入社(アルバイト) / 以降2年間勤務','>デリカフェ・キッチン大阪にてカフェの接客・レジ・調理に従事']
+            },
+            {
+                content: '8月',
+                detail: ['株式会社 王将フードサービス 入社(アルバイト) / 以降1年間半勤務','>餃子の王将福島店にて接客・レジ・調理に従事']
+            },
+            {
+                content: '12月',
+                detail: '2021年度秋学期「立命館大学西園寺記念奨学生(成績優秀者枠)」に選抜'
+            },
+        ]
+    },
+    {
+        theme: '2022',
+        dataList: [
+            {
+                content: '8月',
+                detail: '株式会社クラックスシステム 2weeks体験型インターン'
+            },
+            {
+                content: '10月',
+                detail: ['学生IT団体watnowに入る','日本ソフト開発株式会社 1dayインターン']
+            },
+            {
+                content: '12月',
+                detail: '2021年度秋学期「立命館大学西園寺記念奨学生(成績優秀者枠)」に選抜'
+            },
+        ]
+    },
+    {
+        theme: '2023',
+        dataList: [
+            {
+                content: '2月',
+                detail: '関西ビギナーズハッカソン 優秀賞受賞'
+            },
+            {
+                content: '3月',
+                detail: 'ハックツハッカソン アロカップ 参加'
+            },
+        ]
+    },
+]
+
+export default profileData;

1316690c8bd27b35da3cd4ed35edbc8dee23f0a7
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-10 16:59:37 +0900

    contentCardの余白調整

--- src/components/ContentsCard.tsx
+++ src/components/ContentsCard.tsx
@@ -11,10 +11,10 @@ function ContentsCard(prop: Content) {
   return (
     // <Link href={"/Contents/"+prop.theme}>
     <Link href="/Contents">
-      <div className="max-w-md my-12 bg-white rounded-xl shadow-md overflow-hidden md:max-w-full md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <div className="my-12 bg-white rounded-xl shadow-md overflow-hidden max-w-full md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
         <div className="md:grid md:grid-cols-[2fr_3fr]">
           <div className="m-auto md:shrink-0">
-            <Image className="md:pt-0 pt-4 h-48 w-fit object-contain m-auto" src={prop.img} alt="" width={200} height={200}></Image>
+            <Image className="md:pt-0 pt-4 md:pl-4 pl-0 h-48 w-fit object-contain m-auto" src={prop.img} alt="" width={200} height={200}></Image>
           </div>
           <div className="md:p-8 py-4 px-8">
             <div className={"uppercase tracking-wide text-sm " + (prop.type=="team"?"text-indigo-500":"text-yellow-400") + " font-semibold"}>{prop.type}</div>

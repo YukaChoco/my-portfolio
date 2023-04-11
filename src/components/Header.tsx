d709b76da7c142fcae0fcb7ff5dc70a7c5c71974
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-11 11:47:20 +0900

    headerのタイポ修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import HeaderButton from './HeadeButton'
+import HeaderButton from './HeaderButton'
 
 function Header() {
   return (

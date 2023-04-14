7891fc5101beaf8bfe2aa535f9470bbdddf170d7
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-14 17:36:53 +0900

    コンテンツページ画像部分・テキスト部分のレイアウト調整

--- src/components/ContentDetails.tsx
+++ src/components/ContentDetails.tsx
@@ -1,7 +1,10 @@
+import img from '../../public/tidyIconImg.jpeg'
+import Image from 'next/image'
+
 function ContentDetails() {
   return (
-    <div className='h-screen pt-24 pb-12 px-[10vw] grid grid-cols-2'>
-      nakamidayo
+    <div className='h-screen pt-28 pb-12 px-[10vw] grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1'>
+      <Image className="my-auto max-h-96 h-full w-full object-contain" src={img} alt="" width={200} height={200}></Image>
     </div>
   );
 }

88943a9f81e5de80b24219c19a0948cfb05abb7e
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-14 14:46:42 +0900

    コンテンツページ枠組み完成

---
+++ src/components/ContentDetails.tsx
@@ -0,0 +1,9 @@
+function ContentDetails() {
+  return (
+    <div className='h-screen pt-24 pb-12 px-[10vw] grid grid-cols-2'>
+      nakamidayo
+    </div>
+  );
+}
+
+export default ContentDetails;
--- src/pages/Contents/[contentName].tsx
+++ src/pages/Contents/[contentName].tsx
@@ -1,21 +1,32 @@
 import DisplayContent from '@/components/DisplayContent';
 import Header from '../../components/Header';
-import experienceData from '../../data/experience'
+import ContentDetails from '../../components/ContentDetails'
 import { useRouter } from 'next/router';
+import { Content } from "@/types/global";
+import Data from '../../data/contents';
 
-function Content() {
+function Contents() {
   const router = useRouter();
   const contentName = router.query.contentName as string;
-  return (
-    <div>
-      {contentName}
-    </div>
-    // <ul>
-    //   {contentsName.map(post => (
-    //     <li>{post}</li>
-    //   ))}
-    // </ul>
-  );
+  const contentData: Content[] = Data.filter(content => content.theme === contentName);
+
+  if (contentData[0] !== undefined) {
+    return (
+      <div className='w-full bg-zinc-100 min-h-screen h-fit'>
+        <Header></Header>
+        <ContentDetails></ContentDetails>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className='bg-gray-200 w-screen h-screen text-center flex'>
+        <span className='m-auto w-fit h-fit'>
+          ー URLが間違っています ー
+        </span>
+      </div>
+    )
+  }
 }
 
-export default Content;
+export default Contents;

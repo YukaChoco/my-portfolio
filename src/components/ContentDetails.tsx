f59af8fae3ec5c466718643de590681ed73217a3
YukaChoco <sugar.ore.yk@gmail.com>
2023-05-23 23:47:38 +0900

    各コンテンツページ変更

--- src/components/ContentDetails.tsx
+++ src/components/ContentDetails.tsx
@@ -8,10 +8,14 @@ type Props = {
 
 function ContentDetails({ contentData, key }: Props) {
   return (
-    <div className='pt-28 pb-12 px-[10vw] grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1' key={key}>
-      <Image className="md:mt-16 max-h-96 h-full w-full object-contain" src={contentData.img} alt="" quality={100} width={500} height={500}></Image>
-      <div className='h-fit bg-white/80'>
-        aaaa
+    <div className='pt-28 pb-12 px-[5vw] grid grid-cols-1 md:grid-cols-2  gap-4'>
+      <div className='mb-4 md:my-8 h-full w-full'>
+      <Image className="object-contain max-h-80 h-full w-fit m-auto drop-shadow-2xl" src={contentData.img} alt="" quality={100} width={500} height={500}></Image>
+      </div>
+      <div className='relative h-fit bg-white/50 p-8 z-10'>
+        <div className={"absolute border-2 right-12 top-8 py-1 px-3 rounded-3xl uppercase w-fit tracking-wide text-base " + (contentData.type=="team"?"text-indigo-500":"text-yellow-400") + " " + (contentData.type=="team"?"border-indigo-500":"border-yellow-400") + " font-bold"}>{contentData.type}</div>
+        <div className='pt-8 text-3xl font-bold border-b-2 border-black'>{contentData.theme}</div>
+        <div className='pt-2 pl-2'>{contentData.detail}</div>
       </div>
     </div>
   );

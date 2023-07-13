704516130b4785887d33118d50d30216e22ce481
YukaChoco <sugar.ore.yk@gmail.com>
2023-07-14 00:28:30 +0900

    title,OGP指定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <title>MyPortfolio</title>
+        <meta name="description" content="藤堂ゆうかのポートフォリオサイトです。プロフィールや成果物などをまとめています。" />
+        <meta property="og:title" content="MyPortfolio" />
+        <meta property="og:description" content="藤堂ゆうかのポートフォリオサイトです。プロフィールや成果物などをまとめています。" />
+      </Head>
       <body>
         <Main />
         <NextScript />

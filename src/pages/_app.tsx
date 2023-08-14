1065a0e87c5b85f5e4388f889dbd46d64a3f4e14
YukaChoco <sugar.ore.yk@gmail.com>
2023-08-14 23:01:31 +0900

    fixed Head: _document.tsx -> _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>MyPortfolio</title>
+        <meta name="description" content="藤堂ゆうかのポートフォリオサイトです。プロフィールや成果物などをまとめています。" />
+        <meta property="og:title" content="MyPortfolio" />
+        <meta property="og:description" content="藤堂ゆうかのポートフォリオサイトです。プロフィールや成果物などをまとめています。" />
+        <meta property="og:image" content="/portfolio.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head>
-        <title>MyPortfolio</title>
-        <meta name="description" content="藤堂ゆうかのポートフォリオサイトです。プロフィールや成果物などをまとめています。" />
-        <meta property="og:title" content="MyPortfolio" />
-        <meta property="og:description" content="藤堂ゆうかのポートフォリオサイトです。プロフィールや成果物などをまとめています。" />
-        <meta property="og:image" content="/portfolio.png" />
-      </Head>
       <body>
         <Main />
         <NextScript />
